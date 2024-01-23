4a51f87d0572db1ee5011e65729939c66071d620
Vishalkarthik27 <141307452+Vishalkarthik27@users.noreply.github.com>
2024-01-23 18:29:24 +0530

    committed 1/23/2024, 6:29:23 PM

--- set01/problem02.c
+++ set01/problem02.c
@@ -4,9 +4,9 @@ int main()
 {
     int a,b,sum;
     printf("enter first numbers:");
-    scanf("%d %d",&a);
+    scanf("%d",&a);
      printf("enter second numbers:");
-    scanf("%d %d",&b);
+    scanf("%d",&b);
     sum=a+b;
     printf("the sum of two numbers is: %d",sum);
     return 0;
