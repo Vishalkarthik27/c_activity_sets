a545ad998f21cc4635cc9bf3b7287f1ee781bf3e
Vishalkarthik27 <141307452+Vishalkarthik27@users.noreply.github.com>
2024-01-24 22:02:08 +0530

    committed 1/24/2024, 10:02:08 PM

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,30 +1,49 @@
 #include <stdio.h>
+#include <math.h>
 
-float input() {
+float input()
+{
     float n;
     printf("Enter a number: ");
     scanf("%f", &n);
     return n;
 }
-float square_root(float n) {
-    float x = n / 2;
 
-    for (int i = 0; i < 10; ++i) {
-        x = 0.5 * (x + n / x);
+float square_root(float n)
+{
+
+    if (n < 0)
+    {
+        printf("Square root of a negative number is not defined.\n");
+        return -1.0;
     }
 
-    return x;
+    float guess = n;
+    float previous_guess;
+    float epsilon = 0.0001;
+
+    while (fabs(guess - previous_guess) > epsilon)
+    {
+        previous_guess = guess;
+        guess = 0.5 * (guess + n / guess);
+    }
+
+    return guess;
 }
 
-void output(float n, float sqrroot) {
-    printf("Square root of %.2f is approximately %.4f\n", n, sqrroot);
+void output(float n, float sqrroot)
+{
+    if (sqrroot >= 0)
+    {
+        printf("The square root of %f is approximately %f\n", n, sqrroot);
+    }
 }
 
-int main() {
-    float n, sqrroot;
-    n = input();
-    sqrroot = square_root(n);
-    output(n, sqrroot);
-
+int main()
+{
+    float number, result;
+    number = input();
+    result = square_root(number);
+    output(number, result);
     return 0;
 }
